4b6ee9b97bd2a47a90476d170f10f0d7bfd227dd
szombathyisti <127030170+szombathyisti@users.noreply.github.com>
2025-10-25 16:24:22 +0200

    Add files via upload

--- kodok/PieChart.py
+++ kodok/PieChart.py
@@ -5,35 +5,25 @@ df = px.data.iris()
 fig = px.pie(
     df,
     names="species",
-    title="Iris fajok eloszlása",
     hole=0.0,
     color_discrete_sequence=["#679EE6", "#4D76C1", "#1E3A8A"]
 )
 
-
 fig.update_traces(
     textinfo="percent+label",
     hoverinfo="none",
+    textfont=dict(size=24, family="Arial", color="black"),  # nagyobb betűméret
     marker=dict(line=dict(color="#FFFFFF", width=0.0))
 )
 
 fig.update_layout(
     template="simple_white",
-    showlegend=False,  
-    font=dict(family="Arial", size=14, color="black"),
-    title=dict(
-        text="Iris fajok eloszlása<br><sup>Ronald Fisher adathalmaza alapján</sup>",
-        font=dict(size=20, family="Arial", color="black"),
-        x=0.5,  
-        xanchor="center",
-        yanchor="top",
-        pad=dict(t=50)  
-    ),
+    showlegend=False,
+    font=dict(family="Arial", size=16, color="black"),
     width=800,
     height=600,
-    margin=dict(t=100, l=60, r=30, b=60)  
+    margin=dict(t=10, l=10, r=10, b=10)  # minimális margó
 )
-
 
 fig.write_image("Piechart.png", scale=3)
 print("Mentve: Piechart.png")
